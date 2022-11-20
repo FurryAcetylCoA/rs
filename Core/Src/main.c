53d9408046726a97ccbbcf3d5d1b45dd013497f3
FurryAcetylCoA <1109673069@qq.com>
2022-11-20 17:50:44 +0800

    提升I2C始终速度

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -317,7 +317,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 1*1000;
+  hi2c1.Init.ClockSpeed = 80*1000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -620,5 +620,3 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
-#pragma clang diagnostic pop
